253afb543246da5342ef1b78b1b1656d31142de7
IvanCalvo <ivandc1998.ic@gmail.com>
2023-05-24 11:41:17 -0300

    tamo no caminho 3

--- gerador_lexico/gramatica.g4
+++ gerador_lexico/gramatica.g4
@@ -1,18 +1,27 @@
 lexer grammar gramatica;
 
-PALAVRA_CHAVE 
-	:	'algoritmo' | 'declare' | 'inteiro' | 'leia' | 'escreva' | 'fim_algoritmo' | ':' | '(' | ')'
-	;
+PALAVRA_CHAVE
+    : 'algoritmo' | 'declare' | 'literal' | 'inteiro' | 'real' | 'logico' | 'ou' | 'nao' | 'leia' | 'se' | 'fim_se' 
+    | 'senao' | 'entao' | 'falso' | 'verdadeiro' | 'caso' | 'e' | 'fim_caso' | 'escreva' | 'seja' | 'para' | 'ate' | 'faca' 
+    | 'fim_para' | 'enquanto' | 'fim_enquanto' | 'registro' | 'fim_registro' | 'tipo' | 'procedimento' 
+    | 'var' | 'fim_procedimento' | 'funcao' | 'fim_funcao' | 'retorne' | 'constante' | 'fim_algoritmo'
+    ;
+
+NUM_INT : ('0'..'9')+ ;
+
+NUM_REAL : ('0'..'9')+('.'('0'..'9')+)?;
+
+IDENT
+    : [A-Za-z_][A-Za-z0-9_]*
+    ;
 
 CADEIA
     : '"' ( ESC_SEQ | ~('\''|'\\') )* '"'
 	;
-fragment
-ESC_SEQ	: '\\"';
 
-IDENT
-    : ('a'..'z'|'A'..'Z') ('a'..'z'|'A'..'Z'|'0'..'9')*
-    ;
+fragment
+ESC_SEQ	: '\\\'';
+
 
 COMENTARIO
     :   '{' ~('\n'|'\r')* '\r'? '}' '\n' -> skip
@@ -25,3 +34,21 @@ WS
         | '\n'
         ) -> skip
     ;
+
+DOIS_PONTOS: ':' ;
+
+ABRE_PARENTESES: '(' | '[';
+
+FECHA_PARENTESES: ')' | ']';
+
+VIRGULA: ',' ;
+
+OP_RELACIONAL	:	'>' | '>=' | '<' | '<=' | '<>' | '=';
+
+OP_ATRIBUICAO : '<-';
+
+OP_ACESSO: '.';
+
+OP_ARITMETICO	:	'+' | '-' | '*' | '/' | '%' | '^' | '&';
+
+ERR : ~('a');
--- gerador_lexico/main.py
+++ gerador_lexico/main.py
@@ -3,12 +3,13 @@ from antlr4 import *
 from gramatica2 import gramatica2
 
 # Verifique se foi fornecido o nome do arquivo como argumento
-if len(sys.argv) < 2:
-    print("É necessário fornecer o nome do arquivo de entrada como argumento.")
+if len(sys.argv) < 3:
+    print("É necessário fornecer o nome dos arquivos de entrada e saida como argumento.")
     sys.exit(1)
 
 # Obtém o nome do arquivo a partir dos argumentos
 input_file_name = sys.argv[1]
+output_file_name = sys.argv[2]
 
 try:
     # Crie um objeto FileStream com o arquivo de entrada
@@ -22,6 +23,7 @@ try:
 
     tipos_definidos = ['IDENT', 'CADEIA', 'NUM_INT', 'NUM_REAL']
 
+    output = open(output_file_name, 'w')
 
     # Itere sobre os tokens até encontrar o fim do arquivo
     while token.type != Token.EOF:
@@ -29,22 +31,22 @@ try:
         txt = '\'' + token.text + '\''
         typeStr = gramatica2.symbolicNames[token.type]
         if (typeStr == 'Nao_fechado'):
-            print('Linha ' + token.line + ': comentario nao fechado')
+            output.writelines('Linha ' + token.line + ': comentario nao fechado\n')
             break
 
         elif (typeStr == 'Literal_Nao_Fechada'):
-            print('Linha ' + token.line + ': cadeia literal nao fechada')
+            output.writelines('Linha ' + token.line + ': cadeia literal nao fechada\n')
             break
 
         elif(typeStr == 'ERR'):
-            print('Linha ' + token.line + ': ' + token.text + ' - simbolo nao identificado')
+            output.writelines('Linha ' + token.line + ': ' + token.text + ' - simbolo nao identificado\n')
             break
 
         else:    
             if (typeStr in tipos_definidos):
-                print('<'+ txt + "," + typeStr + '>')
+                output.writelines('<'+ txt + "," + typeStr + '>\n')
             else:
-                print('<' + txt + ',' + txt + '>')
+                output.writelines('<' + txt + ',' + txt + '>\n')
             
         # Obtenha o próximo token
         token = lexer.nextToken()
